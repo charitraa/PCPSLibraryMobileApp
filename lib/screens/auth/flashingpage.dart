8f0564af24f870cc1d231bac1e852f4243c3a74a
anushatamrakar <anusha.tamrakar@patancollege.edu.np>
2025-02-13 11:48:53 +0545

    created flashing page

---
+++ lib/screens/auth/flashingpage.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+class Flashingpage extends StatefulWidget {
+  const Flashingpage({super.key});
+
+  @override
+  State<Flashingpage> createState() => _FlashingpageState();
+}
+
+class _FlashingpageState extends State<Flashingpage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+
+    );
+  }
+}
